b4ab0e1ad724a07935485ed9540cd9d742fad835
ghostxic <mcl14@illinois.edu>
2025-10-17 18:44:08 -0500

    Added filtering dropdown on basic / route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, render_template, url_for
+from flask import Flask, session, render_template, url_for, request, redirect
 import filtering_functions
 import json
 import pandas as pd
@@ -9,24 +9,34 @@ from pandas import json_normalize
 app = Flask(__name__)
 app.secret_key = "eats@uiuc"
 
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        cuisine = request.form["cuisine"]
+        session["cuisine"] = cuisine
+        return redirect(url_for("cuisine"))
+    else:
+        return render_template("home.html")
 
 # filtering by cuisine
-@app.route("/cuisine/<cuisine>", methods=["GET"])
-def cuisine(cuisine):
+@app.route("/cuisine", methods=["GET"])
+def cuisine():
     # Read the JSON file
-    with open('data/restaurants.json', 'r') as f:
-        data = json.load(f)
-    # Create a DataFrame with expanded location fields
-    df = json_normalize(data, record_path='locations',meta=['name', 'cuisine'],)
-    # Filter dataframe by cuisine
-    filtered_df = filtering_functions.filter_cuisine(cuisine, df)
+    if "cuisine" in session:
+        cuisine = session["cuisine"]
+        with open('data/restaurants.json', 'r') as f:
+            data = json.load(f)
+        # Create a DataFrame with expanded location fields
+        df = json_normalize(data, record_path='locations',meta=['name', 'cuisine'],)
+        # Filter dataframe by cuisine
+        filtered_df = filtering_functions.filter_cuisine(cuisine, df)
 
-    # Convert to a JSON-serializable list of dicts (one dict per row)
-    display_list = filtered_df.to_dict(orient="records")
-
-    # Store serializable data in session (optional) and pass to template
-    session["display"] = display_list
-    return render_template("cuisine.html", display=display_list)
+        # Convert to a JSON-serializable list of dicts (one dict per row)
+        display_list = filtered_df.to_dict(orient="records")
+        
+        return render_template("cuisine.html", display=display_list)
+    else:
+        return redirect(url_for("home"))
 
 
 
