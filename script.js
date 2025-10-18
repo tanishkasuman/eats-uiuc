19dac020f0bae0b265a34d2ba91323cb62311800
Deathstar8 <deathstar8@outlook.com>
2025-10-17 20:45:37 -0500

    Use OSM Bright Tilemap

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ var map = L.map('map', {
     zoomControl: false
 }).setView([40.0985, -88.2291], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+    maxZoom: 21,
+    attribution: '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>', 
+}).addTo(map);
+
+/*L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+}).addTo(map);*/
 
 L.control.zoom({
     position: 'topright'
